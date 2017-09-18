3e8613ee1493f6cd9ac50991b8f0e7b7c60d2525
James World <james-world@users.noreply.github.com>
2017-09-18 01:47:25 +0100

    Configure player creation persistence and recovery

--- GameConsole/Actors/PlayerCoordinatorActor.cs
+++ GameConsole/Actors/PlayerCoordinatorActor.cs
@@ -1,25 +1,43 @@
 ﻿using System;
 using Akka.Actor;
+using Akka.Persistence;
 using GameConsole.Messages;
 using Serilog;
 
 namespace GameConsole.Actors
 {
-    public class PlayerCoordinatorActor : ReceiveActor
+    public class PlayerCoordinatorActor : ReceivePersistentActor
     {
-        private static readonly ILogger Log = Serilog.Log.ForContext<PlayerCoordinatorActor>();
+        private new static readonly ILogger Log = Serilog.Log.ForContext<PlayerCoordinatorActor>();
         private const int DefaultStartingHealth = 100;
+
+        public override string PersistenceId => "player-coordinator";
 
         public PlayerCoordinatorActor()
         {
-            Receive<CreatePlayerMessage>(msg =>
+            Command<CreatePlayerMessage>(msg =>
             {
                 Log.Information("Received CreatePlayerMessage for {PlayerName}", msg.PlayerName);
 
-                Context.ActorOf(Props.Create(() => new PlayerActor(msg.PlayerName, DefaultStartingHealth)),
-                    msg.PlayerName);
+
+                Persist(msg, createPlayerMessage =>
+                {
+                    Log.Information("Persisted a CreatePlayerMessage for {PlayerName}", createPlayerMessage.PlayerName);
+
+                    Context.ActorOf(Props.Create(() => new PlayerActor(createPlayerMessage.PlayerName, DefaultStartingHealth)),
+                        createPlayerMessage.PlayerName);
+                });
+            });
+
+            Recover<CreatePlayerMessage>(createPlayerMessage =>
+            {
+                Log.Information("Replaying CreatePlayerMessage for {PlayerName}", createPlayerMessage.PlayerName);
+
+                Context.ActorOf(Props.Create(() => new PlayerActor(createPlayerMessage.PlayerName, DefaultStartingHealth)),
+                    createPlayerMessage.PlayerName);
             });
         }
+
         protected override void PreStart()
         {
             Log.Information("PreStart");
